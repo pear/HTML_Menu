d6aa817a3e8f6c0d5c4e0a34e03ce824f698aea0
Alexey Borzov <avb@php.net>
2004-01-17 12:34:15 +0000

    A new SigmaTree renderer with example Example for previously added DirectTree renderer
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Menu/trunk@148938 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/directtree.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Usage example for HTML_Menu, DirectTree renderer
+ * 
+ * $Id$
+ * 
+ * @package HTML_Menu
+ * @author Alexey Borzov <avb@php.net>
+ */
+
+require_once 'HTML/Menu.php';
+require_once 'HTML/Menu/DirectTreeRenderer.php';
+require_once './data/menu.php';
+
+$types = array('tree', 'sitemap');
+
+$menu =& new HTML_Menu($data);
+$menu->forceCurrentUrl('/item1.2.2.php');
+
+foreach ($types as $type) {
+    echo "\n<h1>Trying menu type &quot;{$type}&quot;</h1>\n";
+    $renderer =& new HTML_Menu_DirectTreeRenderer();
+    $menu->render($renderer, $type);
+    echo $renderer->toHtml();
+}
+?>
---
+++ examples/sigmatree.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Usage example for HTML_Menu with SigmaTree renderer 
+ * 
+ * $Id$
+ * 
+ * @package HTML_Menu
+ * @author Alexey Borzov <avb@php.net>
+ */
+
+require_once 'HTML/Menu.php';
+require_once 'HTML/Menu/SigmaTreeRenderer.php';
+require_once 'HTML/Template/Sigma.php';
+require_once './data/menu.php';
+
+$menu =& new HTML_Menu($data);
+$menu->forceCurrentUrl('/item1.2.2.php');
+
+$types = array('tree', 'sitemap');
+
+$tpl =& new HTML_Template_Sigma('./templates');
+$tpl->loadTemplateFile('sigmatree.html', true, true);
+$renderer =& new HTML_Menu_SigmaTreeRenderer($tpl);
+
+foreach ($types as $type) {
+    $tpl->setVariable('type', $type);
+    $menu->render($renderer, $type);
+    $tpl->parse('type_loop');
+}
+
+$rendererCustom =& new HTML_Menu_SigmaTreeRenderer($tpl, 'tree_');
+$menu->forceCurrentUrl('/item1.2.2.2.php');
+$menu->render($rendererCustom, 'tree');
+
+$tpl->show();
+?>
